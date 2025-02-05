583646b0bf911c55e5595fc110701242be5c9195
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-02-05 17:07:46 +0900

    init

---
+++ src/main/java/com/example/practice_jwt/PracticeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_jwt/PracticeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
